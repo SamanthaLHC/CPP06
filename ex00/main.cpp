b7b5f2c50da4b3f3a01a9e772e1dc6fae3794047
Samantha Le Huec <s.lhuec@gmail.com>
2022-12-17 18:58:29 +0100

    add is_float, makefile et gitignore

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,28 +4,12 @@
 #include <sstream>
 #include <limits>
 
-bool check_int_limits(int int_type)
-{
-	if (int_type <= std::numeric_limits<int>::max() || int_type >= std::numeric_limits<int>::min())
-		return true;
-	else
-		return false;
-}
-
-bool check_float_limits(float float_type)
-{
-	if (float_type <= std::numeric_limits<float>::max() || float_type >= std::numeric_limits<float>::min())
-		return true;
-	else
-		return false;
-}
-
 bool is_int(std::string input_to_parse)
 {
 	int int_type = 0;
 
 	std::istringstream ss(input_to_parse);
-	if (ss >> int_type && (check_int_limits(int_type)))
+	if (ss >> int_type && (ss.rdbuf()->in_avail() == 0))
 	{
 		std::cout << "int: " << input_to_parse << std::endl;
 		return true;
@@ -34,59 +18,57 @@ bool is_int(std::string input_to_parse)
 		// execute la method pour convertir en float double et char
 		return false;
 }
-// ATTENTION NOT MINE A DELETE
-bool isInteger(double N)
+
+bool is_inff_or_nanf(std::string input, float &float_type)
 {
-
-	// Convert float value
-	// of N to integer
-	int X = static_cast<double> (N);
-
-	double temp2 = N - X;
-
-	// If N is not equivalent
-	// to any integer
-	if (temp2 > 0)
+	// std::string a une surcharge d'opé qui permet les comp directes
+	if (input == "inff" || input == "+inff")
 	{
-		return false;
+		float_type = std::numeric_limits<float>::infinity();
+		std::cout << "float: " << float_type << "f" << std::endl;
+		return true;
 	}
-	return true;
+	else if (input == "-inff")
+	{
+		float_type = -std::numeric_limits<float>::infinity();
+		std::cout << "float: " << float_type << "f" << std::endl;
+		return true;
+	}
+	else if (input == "nanf")
+	{
+		float_type = std::numeric_limits<float>::signaling_NaN();
+		std::cout << "float: " << float_type << "f" << std::endl;
+		return true;
+	}
+	return false;
 }
 
-// un int peutr etre contenu ds un float
-// un float peut etre contenu ds un double
 bool is_float(std::string input_to_parse)
 {
 	float float_type;
 
 	std::istringstream ss(input_to_parse);
-	if (ss >> float_type && (check_float_limits(float_type)))
+	if (is_inff_or_nanf(input_to_parse, float_type))
+		return true;
+	else if ((ss >> float_type && ss.rdbuf()->sgetc() == 'f'))
 	{
-		std::cout << "Seem to be a float" << std::endl;
-		// -inff +inff nan et limits float ?
+		std::cout << "float: " << input_to_parse << std::endl;
 		return true;
 	}
-	else
-		// call method convert char int double
-		return false;
+	// call method convert char int double
+	return false;
 }
 
 int main(int ac, char **av)
 {
-	(void) ac;
-	(void) av;
-	double N = 1.5;
-	if (isInteger(N))
-		std::cout << "yes" << std::endl;
+
+	if (ac != 2)
+		std::cout << "You have to pass one str as param." << std::endl;
 	else
-		std::cout << "no" << std::endl;
-	// if (ac != 2)
-	// 	std::cout << "You have to pass one str as param." << std::endl;
-	// else
 	{
-		// bool result;
-		// std::string input = av[1];
-		// result = is_float(input);
-		// std::cout << result << std::endl;
+		bool result;
+		std::string input = av[1];
+		result = is_float(input);
+		std::cout << result << std::endl;
 	}
 }
