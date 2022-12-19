d2f78cc24b54692928be11826fc612efbbbef535
Samantha Le Huec <s.lhuec@gmail.com>
2022-12-19 18:27:42 +0100

    first finish

---
+++ ex02/includes/Base.hpp
@@ -0,0 +1,9 @@
+#ifndef BASE_HPP
+#define BASE_HPP
+
+#include <string>
+#include <iostream>
+
+class Base {public: virtual ~Base() {}};
+
+#endif
---
+++ ex02/srcs/main.cpp
@@ -0,0 +1,80 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+#include "colors.h"
+
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+
+Base *generate(void)
+{
+	int generate = 0;
+	std::srand(std::time(NULL));
+	generate = (std::rand() % 3);
+
+	if (generate == 0)
+	{
+		std::cout << BWHT << "create A class" << RES << std::endl;
+		return new A;
+	}
+	else if (generate == 1)
+	{
+		std::cout << BWHT << "create B class" << RES << std::endl;
+		return new B;
+	}
+	else
+	{
+		std::cout << BWHT << "create C class" << RES << std::endl;
+		return new C;
+	}
+}
+
+void identify(Base *p)
+{
+	if (dynamic_cast<A *>(p))
+		std::cout << BWHT << "object is type A " << RES << std::endl;
+	else if (dynamic_cast<B *>(p))
+		std::cout << BWHT << "object is type B " << RES << std::endl;
+	else if (dynamic_cast<C *>(p))
+		std::cout << BWHT << "object is type C" << RES << std::endl;
+	else
+		std::cout << BRED << "ERROR " << RES << std::endl;
+}
+
+void identify(Base &p)
+{
+	try
+	{
+		dynamic_cast<A &>(p);
+		std::cout << BWHT << "object is type A " << RES << std::endl;
+	}
+	catch (const std::exception &)
+	{
+		try
+		{
+			dynamic_cast<B &>(p);
+			std::cout << BWHT << "object is type B " << RES << std::endl;
+		}
+		catch (const std::exception &)
+		{
+			try
+			{
+				dynamic_cast<C &>(p);
+				std::cout << BWHT << "object is type C " << RES << std::endl;
+			}
+			catch (const std::exception &) {}
+		}
+	}
+}
+
+int main()
+{
+	Base *gen = generate();
+	identify(gen);
+	// le prototype attend un ref donc le compilo passe une ref plutot qu une copie
+	identify(*gen);
+	delete gen;
+}
