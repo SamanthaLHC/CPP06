3749a38eb32a7f7da18caaf530f3703fd4245fca
Samantha Le Huec <s.lhuec@gmail.com>
2022-12-19 15:54:40 +0100

    ex01 done

---
+++ ex01/includes/data.hpp
@@ -0,0 +1,13 @@
+#ifndef DATA_HPP
+#define DATA_HPP
+
+#include <string>
+
+struct Data
+{
+	int i;
+	float f;
+	std::string str;
+};
+
+#endif
---
+++ ex01/srcs/main.cpp
@@ -0,0 +1,34 @@
+#include "data.hpp"
+#include "colors.h"
+
+#include <stdint.h>
+#include <iostream>
+
+uintptr_t serialize(Data *ptr)
+{
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data *deserialize(uintptr_t raw)
+{
+	return reinterpret_cast<Data *>(raw);
+}
+
+int main()
+{
+	Data *data = new Data;
+	data->i = 42;
+	data->f = 8.0f;
+	data->str = "pouet";
+	std::cout << BWHT << "int in data: " << data->i << std::endl
+			  << BWHT << "float in data: " << data->f << std::endl
+			  << BWHT << "str in data: " << data->str << RES << std::endl;
+	uintptr_t ret = serialize(data);
+	Data *data_serialized = deserialize(ret);
+	std::cout << BCYN << "data's ptr:  " << data << std::endl
+			  << BCYN << "ret deserialize: " << data_serialized << RES << std::endl;
+	std::cout << BWHT << "int in data_serialized: " << data_serialized->i << std::endl
+			  << BWHT << "float in data_serialized: " << data_serialized->f << std::endl
+			  << BWHT << "str in data_serialized: " << data_serialized->str << RES << std::endl;
+	delete data;
+}
